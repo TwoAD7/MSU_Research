92a367590b72cae5842a1914aea576d8c195c71e
Roy Salinas <Roy-123456@hotmail.com>
2020-07-29 13:50:47 -0500

    Update auto4LISE.py

--- auto4LISE.py
+++ auto4LISE.py
@@ -13,9 +13,9 @@ def main():
 	print("Beginning the automation for LISE++...")
 	res = input("Are you opening the program for the first time? (yes or no) ")
 	if res == "yes":
-		bool_val,FP_slit_width,isotope_start,wedge_range=a4l.start() #NEED TO UPDATE TO INCLUDE ISOTOPE_END
+		FP_slit_width,isotope_start,isotope_end,wedge_range=a4l.start()  
 	else:
-		bool_val,FP_slit_width,isotope_start,isotope_end,wedge_range=a4l.start2()
+		FP_slit_width,isotope_start,isotope_end,wedge_range=a4l.start2()
 	#a4l.show_pixels()
 	#a4l.FP_slit_X_transmission_percent()
 	#a4l.start()
@@ -25,7 +25,8 @@ def main():
 	#a4l.save()
 	#a4l.isotope_loop()
 	#a4l.get_intensity("Mg_35","Ar",36)
-	a4l.isotope_tuning_values(bool_val,FP_slit_width,isotope_start,isotope_end,wedge_range)
+	a4l.isotope_tuning_values(FP_slit_width,isotope_start,isotope_end,wedge_range)
 	#a4l.purity_percent("Mg_36")
 	#a4l.set_I2_wedge(2300)
+	
 main()
